dbf74c42e148313ae74f02da3215e28383adcdef
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2020-12-03 17:02:16 +0100

    Declare imports explicitly

--- 2020/2/solve_p1.hs
+++ 2020/2/solve_p1.hs
@@ -4,7 +4,7 @@
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-import Data.Text.Read
+import Data.Text.Read (decimal)
 
 data PasswordCheck = Check Int Int Char T.Text
                    deriving (Show)
