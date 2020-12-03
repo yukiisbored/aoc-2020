6d69d76b06e7d8089694f07d1d5a14ecd7f6b04a
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2020-12-03 17:37:51 +0100

    flake8 pass

--- 2020/3/solve_p2.py
+++ 2020/3/solve_p2.py
@@ -2,6 +2,7 @@
 
 from functools import partial
 
+
 def step(take_x, y):
     return (take_x * y % 31, y)
 
@@ -10,19 +11,28 @@ def navigate(step_func, target_y):
     return [step_func(y) for y in range(1, target_y)]
 
 
-first_case  = partial(navigate, partial(step, 1))
+first_case = partial(navigate, partial(step, 1))
+
+
 second_case = partial(navigate, partial(step, 3))
-third_case  = partial(navigate, partial(step, 5))
+
+
+third_case = partial(navigate, partial(step, 5))
+
+
 fourth_case = partial(navigate, partial(step, 7))
+
 
 def fifth_case(target_y):
     return [(y // 2 % 31, y) for y in range(2, target_y, 2)]
+
 
 def count_trees_of(world, navigate_func):
     points = navigate_func(len(world))
     traversed_tiles = map(lambda p: world[p[1]][p[0]], points)
 
     return sum(1 for tile in traversed_tiles if tile == '#')
+
 
 def main():
     world = open('in.txt', 'r').readlines()
